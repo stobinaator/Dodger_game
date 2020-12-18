e45c1672041bb951d1ee50541f19d979201e254e
stobko <stoyan.georgiev95@gmail.com>
2020-12-18 12:03:20 +0200

    fixed the scoring, and saving the highest score

--- le_game.py
+++ le_game.py
@@ -66,7 +66,6 @@ speedUpImage = pygame.image.load(PLAYERS_DIR + 'powerups/' + 'flash.png').conver
 
 
 def load_data(DIRECTORY, HS_FILE):
-    #directory = os.path.dirname(__file__)
     with open(os.path.join(DIRECTORY, HS_FILE), 'r') as f:
         try:
             highscore = float(f.read())
@@ -187,15 +186,15 @@ def addNewFigure(image):
 
 def movePlayerAround(moveLeft, moveRight, moveUp, moveDown, playerRect, PLAYERMOVERATE):
     # move the player around
-    if moveLeft: 
+    if moveLeft:
             playerRect.move_ip(-1 * PLAYERMOVERATE, 0)
-            if playerRect.right < 0:
-                playerRect.x = WINDOW_WIDTH
+            if playerRect.centerx - 10 < 0:
+                playerRect.x = WINDOW_WIDTH - 10
                 playerRect.y = playerRect.y
-    if moveRight: 
+    if moveRight:
             playerRect.move_ip(PLAYERMOVERATE, 0)
-            if playerRect.left > WINDOW_WIDTH:
-                playerRect.x = 0
+            if playerRect.centerx + 10 > WINDOW_WIDTH:
+                playerRect.x = 10
                 playerRect.y = playerRect.y
     if moveUp and playerRect.top > 0:
             playerRect.move_ip(0, -1 * PLAYERMOVERATE)
@@ -209,26 +208,27 @@ def main_menu():
     windowSurface.fill(BACKGROUNDCOLOR)
     drawText('∆ Antisocial ∆', font, windowSurface, (WINDOW_WIDTH/3) , (WINDOW_HEIGHT/3))
     hs = load_data(DIRECTORY, HS_FILE)
-    drawText(f'HIGH SCORE: {hs}', font, windowSurface, (WINDOW_WIDTH/3) + 10, (WINDOW_HEIGHT/3) + 100)
+    drawText(f'HIGH SCORE: {hs}', font, windowSurface, (WINDOW_WIDTH/3) - 50, (WINDOW_HEIGHT/3) + 100)
     pygame.display.update()
     waitForPlayerToPressKey2()
     main_menu2()
     return hs
 
+TEXT_POSITION = 70
 
 def main_menu2():
     windowSurface.fill(BACKGROUNDCOLOR)
     drawText('There are 7 stages',
-         font, windowSurface, 110, (WINDOW_HEIGHT/4))
+         font, windowSurface, TEXT_POSITION, (WINDOW_HEIGHT/4))
     drawText('Use arrows or wasd',
-         font, windowSurface, 110, (WINDOW_HEIGHT/4) + 50)
+         font, windowSurface, TEXT_POSITION, (WINDOW_HEIGHT/4) + 50)
     drawText('X to slow down time',
-         font, windowSurface, 110, (WINDOW_HEIGHT/4) + 100)
+         font, windowSurface, TEXT_POSITION, (WINDOW_HEIGHT/4) + 100)
     drawText('Y to make them fly backwards',
-         font, windowSurface, 110, (WINDOW_HEIGHT/4) + 150)
+         font, windowSurface, TEXT_POSITION, (WINDOW_HEIGHT/4) + 150)
     drawText('M to toogle music on/off',
-         font, windowSurface, 110, (WINDOW_HEIGHT/4) + 200)
-    drawText('Press a key to start', font, windowSurface, 110,
+         font, windowSurface, TEXT_POSITION, (WINDOW_HEIGHT/4) + 200)
+    drawText('Press a key to start', font, windowSurface, TEXT_POSITION,
                                                      (WINDOW_HEIGHT/4) + 250)
     pygame.display.update()
     waitForPlayerToPressKey()   
@@ -281,13 +281,11 @@ def checkScore(baddieAddCounter, goodieAddCounter, score, baddies, goodies, badd
         
     elif score < 500:
         if baddieAddCounter == baddieRates['one']['ADDNEWBADDIERATE1']:
-            # print('FIRST 15')
             baddieAddCounter = 0
             newBaddie = addNewFigure(baddieImage)
             baddies.append(newBaddie)
 
         if goodieAddCounter == baddieRates['one']['ADDNEWGOODIERATE1']:
-            # print('FIRST 24')
             goodieAddCounter = 0
             newGoodie = addNewFigure(goodieImage)
             goodies.append(newGoodie)
@@ -313,26 +311,30 @@ def game_over(score, lastScore, topScore, hs):
     pygame.mixer.music.stop()
     gameOverSound.play()
 
-    drawText('GAME OVER', font, windowSurface, (WINDOW_WIDTH /3),
-                                                 (WINDOW_HEIGHT / 3))
+    drawText('GAME OVER', font, windowSurface, (WINDOW_WIDTH /3), (WINDOW_HEIGHT / 3))
     drawText(f'Score: {round(score,2)}', font, windowSurface,
-                 (WINDOW_WIDTH / 3) + 20, (WINDOW_HEIGHT / 3) + 40)
+                 (WINDOW_WIDTH / 3), (WINDOW_HEIGHT / 3) + 40)
     drawText('Press a key to play again.', font, windowSurface,
-                 (WINDOW_WIDTH / 3) - 80, (WINDOW_HEIGHT / 3) + 80)
+                 (WINDOW_WIDTH / 3) - 80, (WINDOW_HEIGHT / 3) + 120)
     
-    if score > hs:
+    print(f'score: {score}, topScore: {topScore}, hs: {hs}')
+
+    if score > topScore:
         topScore = score
+
+    if score > hs:
+        hs = score
         drawText('NEW HIGH SCORE!', font, windowSurface,
-                  (WINDOW_WIDTH / 3) - 80, (WINDOW_HEIGHT / 3) + 120)
+                  (WINDOW_WIDTH / 3) - 40, (WINDOW_HEIGHT / 3) + 80)
         with open(os.path.join(DIRECTORY, HS_FILE), 'w') as f:
-            f.write(str(score) + '\n')
+            f.write(str(hs) + '\n')
     lastScore = score
     pygame.display.update()
     waitForPlayerToPressKey()
 
     gameOverSound.stop()
 
-    return score, lastScore, topScore
+    return score, lastScore, topScore, hs
 
 def game(PLAYERMOVERATE, hs):
     lastScore = 0.0
@@ -477,8 +479,6 @@ def game(PLAYERMOVERATE, hs):
 
             # check if any baddies have hit the player
             if playerHasHitBaddie(playerRect, baddies):
-                if score > topScore:
-                    topScore = score
                 break
                 
 
@@ -498,7 +498,7 @@ def game(PLAYERMOVERATE, hs):
             mainClock.tick(FPS)
 
         # stop the game and show GAME OVER
-        score, lastScore, topScore = game_over(score, lastScore, topScore, hs)
+        score, lastScore, topScore, hs = game_over(score, lastScore, topScore, hs)
 
 
 def main():
